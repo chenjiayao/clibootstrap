1cf926c0afa375efdbc1b82676cc03c989f72fe9
chenjiayao <chenjiayaooo@gmail.com>
2022-04-20 11:21:54 +0800

    :sparkles: (all) add version command

--- cmd/root.go
+++ cmd/root.go
@@ -1,5 +1,30 @@
 package cmd
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "bootstrap",
+	Short: "cli 项目快速开发手脚架",
+	Long: `A Fast and Flexible Static Site Generator built with
+				  love by spf13 and friends in Go.
+				  Complete documentation is available at http://hugo.spf13.com`,
+	Run: func(cmd *cobra.Command, args []string) {
+		Start()
+	},
+}
+
 func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func Start() {
 
 }
